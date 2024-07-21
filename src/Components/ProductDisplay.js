6741a32db72c1c12c468ab8e704f1fb788590e16
Prathmesh Doddanawar <37949352+prathmesh3235@users.noreply.github.com>
2024-07-21 22:17:32 +0200

    window scroll

--- src/Components/ProductDisplay.js
+++ src/Components/ProductDisplay.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { BsDot } from "react-icons/bs";
@@ -7,19 +7,17 @@ import { useNavigate } from "react-router-dom";
 import { doc, setDoc, arrayUnion } from "@firebase/firestore";
 import { db } from "../services/firebase";
 
-
 const ProductDisplay = ({ product, userId, mode, timeData }) => {
   const navigate = useNavigate();
-  window.scrollTo(0, 0);
 
-  const handleClick = () => {
+  const handleClick = async () => {
     const ref = doc(db, "users", userId);
-    let data = {
+    const data = {
       "Clicked More Information": arrayUnion(product.product_name + " " + new Date()),
-      "Time Spent on Presentation Section": timeData
+      "Time Spent on Presentation Section": timeData,
     };
     try {
-      setDoc(ref, data, { merge: true });
+      await setDoc(ref, data, { merge: true });
     } catch (err) {
       console.log(err);
     }
@@ -30,10 +28,14 @@ const ProductDisplay = ({ product, userId, mode, timeData }) => {
 
     navigate(`/product/moreinfo?mode=${mode}&product_id=${product.id}&userId=${userId}&isGoodVersion=${nextVersion}`);
   };
-  
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <Wrapper className="prodDispSec">
-      <h2 className="heading"> Features</h2>
+      <h2 className="heading">Features</h2>
       <div
         style={{
           display: "flex",
@@ -46,56 +48,19 @@ const ProductDisplay = ({ product, userId, mode, timeData }) => {
           <ul className="product-info">
             <li className="product-info-i">
               <h3>
-                {" "}
-                <BsDot /> 	Price: $89.99{" "}
+                <BsDot /> Price: $89.99
               </h3>
             </li>
             <li className="product-info-i">
               <h3>
-                <BsDot />	Color: {product.farbe}{" "}
-              </h3>
-            </li>
-            {/* <li className="product-info-i">
-              <h3>
-                {" "}
-                <BsDot /> Material: metal{" "}
+                <BsDot /> Color: {product.farbe}
               </h3>
             </li>
             <li className="product-info-i">
               <h3>
-                {" "}
-                <BsDot />	Lenses: quality glasses{" "}
-              </h3>
-            </li>
-            <li className="product-info-i">
-              <h3>
-                {" "}
-                <BsDot /> Sizes: small, medium, large{" "}
-              </h3>
-            </li>
-            <li className="product-info-i">
-              <h3>
-                {" "}
-                <BsDot />	Free shipping and return{" "}
-              </h3>
-            </li> */}
-            {/* <li className="product-info-i">
-        
-       <h3><AiOutlineArrowRight/> Extra Feature One </h3>
-      </li>
-      <li className="product-info-i">
-        
-       <h3><AiOutlineArrowRight/> Extra Feature Two </h3>
-      </li> */}
-            <li className="product-info-i">
-              <h3>
-                {" "}
-                <a>
-                  <Button onClick={handleClick}>
-                    {" "}
-                    <AiOutlineArrowRight /> More Information{" "}
-                  </Button>
-                </a>{" "}
+                <Button onClick={handleClick}>
+                  <AiOutlineArrowRight /> More Information
+                </Button>
               </h3>
             </li>
           </ul>
@@ -104,49 +69,43 @@ const ProductDisplay = ({ product, userId, mode, timeData }) => {
     </Wrapper>
   );
 };
+
 const Wrapper = styled.section`
-
-
-position: relative;
-padding:10px;
-margin-top:10px;
-margin-right:10px;
-  .productDisplay: {
-    display: 'flex',
-    flexDirection: 'row';
-    
-  }
-.display-info: {
-
-}
-
-.heading{
-  // font-family: fantasy;
-
-  margin-top: 0px;
-  text-align: center;
-  text-bold: bold;
-  // margin-left:50px;
-  
-  text-decoration-color: Blue;
-  text-decoration-thickness: 5px;
-  font-weight: bold;
-}
-
-
-.product-info: {
-  alignItems: 'center',
-  margin-top:30px;
-}
-.img-style{
+  position: relative;
+  padding: 10px;
   margin-top: 10px;
-  width: 40%;
-  height: auto;
-  display: flex;
-  justify-content: left;
-  align-items: left;
-}
+  margin-right: 10px;
 
+  .productDisplay {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .display-info {
+  }
+
+  .heading {
+    margin-top: 0px;
+    text-align: center;
+    font-weight: bold;
+    text-decoration: underline;
+    text-decoration-color: blue;
+    text-decoration-thickness: 5px;
+  }
+
+  .product-info {
+    align-items: center;
+    margin-top: 30px;
+  }
+
+  .img-style {
+    margin-top: 10px;
+    width: 40%;
+    height: auto;
+    display: flex;
+    justify-content: left;
+    align-items: left;
+  }
 `;
 
 export default ProductDisplay;
